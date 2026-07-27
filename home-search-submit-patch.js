49d0c044a707ecac029345d466b313b3e3e55b25
tiagosaude10-jpg <tiagosaude10@gmail.com>
2026-07-27 09:39:08 -0400

    Corrige recorte da lupa e espaçamento do texto na busca inicial

--- home-search-submit-patch.js
+++ home-search-submit-patch.js
@@ -1,6 +1,6 @@
 (function(){
   'use strict';
-  var PATCH_ID='gestamed-home-search-submit-2026-07-27-180';
+  var PATCH_ID='gestamed-home-search-submit-2026-07-27-181';
   if(document.documentElement.getAttribute('data-gm-home-search-submit')===PATCH_ID)return;
   document.documentElement.setAttribute('data-gm-home-search-submit',PATCH_ID);
   function install(){
@@ -10,9 +10,9 @@
     field.setAttribute('autocomplete','off');
     field.style.setProperty('background','#ffffff','important');
     field.style.setProperty('border-radius','0','important');
-    field.style.setProperty('left','11.5%','important');
-    field.style.setProperty('width','73.5%','important');
-    field.style.setProperty('padding-left','3.2%','important');
+    field.style.setProperty('left','13.2%','important');
+    field.style.setProperty('width','71.8%','important');
+    field.style.setProperty('padding-left','1.8%','important');
     field.style.setProperty('padding-right','2%','important');
     field.style.setProperty('box-sizing','border-box','important');
     field.style.setProperty('color','#3b2333','important');
