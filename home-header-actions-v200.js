ec7ca3b106a2be697e25ddd84f892803da9def2a
tiagosaude10-jpg <tiagosaude10@gmail.com>
2026-08-17 13:11:46 -0400

    Carrega busca unificada sem autocomplete duplicado

--- home-header-actions-v200.js
+++ home-header-actions-v200.js
@@ -1,13 +1,13 @@
 (function () {
   'use strict';
 
-  var PATCH_ID = 'gestamed-header-actions-2026-08-17-218';
+  var PATCH_ID = 'gestamed-header-actions-2026-08-17-219';
   var IIC_MODULE_SRC = 'iic-cerclage-progesterone-module-v211.js?v=20260817-211';
   var ADMIN_ACCESS_FIX_SRC = 'admin-cid-labor-fix-v212.js?v=20260817-212';
   var RESOURCE_DEDUPE_SRC = 'resource-buttons-dedupe-v213.js?v=20260817-213';
   var ADMIN_MANAGER_SRC = 'admin-access-manager-v214.js?v=20260817-214';
   var HOME_GREETING_TEXT_SRC = 'home-greeting-text-v215.js?v=20260817-215';
-  var HOME_MED_SEARCH_FIX_SRC = 'home-medication-search-mobile-fix-v215.js?v=20260817-218';
+  var HOME_MED_SEARCH_FIX_SRC = 'home-medication-search-mobile-fix-v215.js?v=20260817-219';
 
   function applyHeaderActions() {
     var header = document.querySelector('#gm-app-flow .gm-command-header');
@@ -51,7 +51,7 @@
   loadOnce('gm-resource-buttons-dedupe-loader',RESOURCE_DEDUPE_SRC,'resource-buttons-dedupe-v213');
   loadOnce('gm-admin-access-manager-loader',ADMIN_MANAGER_SRC,'admin-access-manager-v214');
   loadOnce('gm-home-greeting-text-loader',HOME_GREETING_TEXT_SRC,'home-greeting-text-v215');
-  loadOnce('gm-home-med-search-fix-loader',HOME_MED_SEARCH_FIX_SRC,'home-medication-search-v218');
+  loadOnce('gm-home-med-search-fix-loader',HOME_MED_SEARCH_FIX_SRC,'home-medication-search-v219');
 
   var attempts = 0;
   function start() {
