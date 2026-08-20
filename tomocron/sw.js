8cb8252fefbd5eb54bf83140d4de919c6e6fe2df
tiagosaude10-jpg <tiagosaude10@gmail.com>
2026-08-19 20:08:24 -0400

    Aplicar ícone final aprovado do TomoCron

--- tomocron/sw.js
+++ tomocron/sw.js
@@ -1,5 +1,5 @@
-const CACHE='tomocron-v4-2026-08-19';
-const ASSETS=['./','./index.html','./app.css?v=4','./app.js?v=4','./manifest.webmanifest','./icon-180.png','./icon-192.png','./icon-512.svg','./icon-maskable-512.svg','./logo-tomocron.svg'];
+const CACHE='tomocron-v5-icon-final-2026-08-19';
+const ASSETS=['./','./index.html','./app.css','./app.js','./manifest.webmanifest','./icon-180.png','./icon-192.png','./icon-512.svg','./icon-maskable-512.svg'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))));self.clients.claim()});
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))))});
