d1ef519621be3f8b6db8ea2c877486aed7f9874d
tiagosaude10-jpg <tiagosaude10@gmail.com>
2026-08-21 08:06:38 -0400

    GestaMed: carregar bloco F sobre main atualizado

--- home-header-actions-v200.js
+++ home-header-actions-v200.js
@@ -1,7 +1,7 @@
 (function () {
   'use strict';
 
-  var PATCH_ID = 'gestamed-header-actions-2026-08-21-265';
+  var PATCH_ID = 'gestamed-header-actions-2026-08-21-266';
   var IIC_MODULE_SRC = 'iic-cerclage-progesterone-module-v211.js?v=20260817-211';
   var HOME_GREETING_TEXT_SRC = 'home-greeting-text-v215.js?v=20260817-215';
   var HOME_MED_SEARCH_FIX_SRC = 'home-medication-search-mobile-fix-v215.js?v=20260817-301';
@@ -29,6 +29,7 @@
   var HYPERTENSIVE_EMERGENCY_SRC = 'hypertensive-disorders-emergency-v262.js?v=20260821-262';
   var HYPERTENSIVE_EMERGENCY_RESET_SRC = 'hypertensive-disorders-emergency-reset-v263.js?v=20260821-263';
   var HYPERTENSIVE_HELLP_SURVEILLANCE_SRC = 'hypertensive-disorders-hellp-surveillance-v264.js?v=20260821-264';
+  var HYPERTENSIVE_DELIVERY_POSTPARTUM_SRC = 'hypertensive-disorders-delivery-postpartum-v265.js?v=20260821-265';
 
   function applyHeaderActions() {
     var header = document.querySelector('#gm-app-flow .gm-command-header');
@@ -93,6 +94,7 @@
   loadOnce('gm-hypertensive-emergency-loader',HYPERTENSIVE_EMERGENCY_SRC,'hypertensive-disorders-emergency-v262');
   loadOnce('gm-hypertensive-emergency-reset-loader',HYPERTENSIVE_EMERGENCY_RESET_SRC,'hypertensive-disorders-emergency-reset-v263');
   loadOnce('gm-hypertensive-hellp-surveillance-loader',HYPERTENSIVE_HELLP_SURVEILLANCE_SRC,'hypertensive-disorders-hellp-surveillance-v264');
+  loadOnce('gm-hypertensive-delivery-postpartum-loader',HYPERTENSIVE_DELIVERY_POSTPARTUM_SRC,'hypertensive-disorders-delivery-postpartum-v265');
 
   var attempts = 0;
   function start() {
